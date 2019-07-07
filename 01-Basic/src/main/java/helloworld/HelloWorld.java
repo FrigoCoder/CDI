bcf2b92c520cfbfd06ed7b87b49ad8a0a2bd4838
Frigo <frigocoder@gmail.com>
2019-07-07 23:58:44 +0200

    Added non-functioning skeleton.

---
+++ 01-Basic/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+
+package helloworld;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@ApplicationScoped
+@Path("")
+public class HelloWorld {
+
+    @GET
+    @Path("hello")
+    public String hello () {
+        return "Hello World!";
+    }
+
+}
